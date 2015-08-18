cc714a1daaba7f5b9d3cef74ffa78de5b6ccc40d
AnarchyInTheUSA <jrtf83@gmail.com>
2015-08-17 21:46:51 -0700

    Finishing intro to JD class

--- Week4 8-17-15/script.js
+++ Week4 8-17-15/script.js
@@ -5,8 +5,31 @@ var sum = addend1 + addend2;
 
 console.log(sum);
 
-alert("The sum is " + sum);
+/* alert("The sum is " + sum);
 
 var isItRight = confirm("Is the sum 15?"); 
 
 sum = prompt("What is the sum?");
+
+var popup = alert;
+
+popup */
+
+var age = prompt("How old are you?");
+
+age = parseInt(age, 10);
+
+if (isNaN(age)) {
+	alert("You screwed up!");
+} else if (age < 100) {
+	alert("You're a child!");
+}
+else if (age === 29){
+	alert("That's my age!");
+}
+else {
+	alert("You're an old fart!")
+}
+
+
+
